3741e496ede7f91c596be9e7c4654a7f564d268a
absognety <vikasch.1994@gmail.com>
2019-01-19 19:46:44 +0530

    merge the tools

--- mergeTools.py
+++ mergeTools.py
@@ -1,12 +1,16 @@
-def merge_tools():
-    
-    
-    
-    
-    
-    
-    
+from collections import OrderedDict
+
+def merge_tools(STR,K):
+    len_S = len(STR)
+    segment_list = []
+    C=0
+    while (C < len_S):
+        segment_list.append(STR[C:C+K])
+        C+=K
+    for seg in segment_list:
+        print (''.join(OrderedDict.fromkeys(seg)))
     
     
 if __name__ == '__main__':
-    
+    string, k = input(), int(input())
+    merge_tools(string, k)

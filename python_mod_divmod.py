a861443b1034076b0fc3fca2007ffc9e870e51a6
absognety <vikasch.1994@gmail.com>
2019-02-10 13:50:15 +0530

    python - div, divmod

---
+++ python_mod_divmod.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import division
+a = int(input())
+b = int(input())
+
+print (a//b)
+print (a%b)
+print (divmod(a,b))
+

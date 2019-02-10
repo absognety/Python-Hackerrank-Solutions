221c0eb94273e73e1c7fa25bba655ae80909f0e5
absognety <vikasch.1994@gmail.com>
2019-02-10 15:48:51 +0530

    Hex Color Code - regex and parsing

---
+++ Hex_Color_Code.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import re, sys
+[print(j) for i in sys.stdin for j in re.findall('[\s:](#[a-f0-9]{6}|#[a-f0-9]{3})', i, re.I)]
+
